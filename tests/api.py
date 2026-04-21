958a864d58261d6705aa73c310e495ebc37da677
Alan Bianchi <alanbianchi@coppe.ufrj.br>
2026-04-21 09:23:59 -0300

    criando recuperação de senha

--- tests/api.py
+++ tests/api.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBFcGgwc9scVHLWr0zNt5t79Lc-HuoiK_A")
+client = genai.Client(api_key="")
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents="Responda em uma frase: O que é uma API?"
